94007b61adb459a05263e0298a2bd89f6b3d2cd6
Pedro Henrique <pdr_castro@hotmail.com>
2021-02-16 16:21:57 -0300

    change folder structure and add js to image upload with wp media

--- wp-content/plugins/ads/ads.php
+++ wp-content/plugins/ads/ads.php
@@ -59,27 +59,35 @@ function adsAdminPage()
     $delete_id = $_GET['delete'];
     $wpdb->query("DELETE FROM $table_name WHERE id='$delete_id'");
   }
-
+  
+  wp_enqueue_script('jquery');
+  
+  wp_enqueue_media();
 ?>
 
   <div class="wrap">
+    <script src="../wp-content/plugins/ads/script.js"></script>
+
     <h2>Gerenciar Anuncios</h2>
     <table class="wp-list-table widefat striped">
       <thead>
         <tr>
+        <th width="25%">Imagem</th>
           <th width="25%">Nome</th>
           <th width="25%">Descrição</th>
           <th width="25%">Tag</th>
-          <th width="25%">Imagem</th>
         </tr>
       </thead>
       <tbody>
         <form action="" method="post">
           <tr>
+            <td>
+                <input type="text" id="new-image" name="imagepath">
+                <input type="button" name="upload-btn" id="upload-btn" class="button-secondary" value="Upload Image">
+            </td>
             <td><input type="text" id="new-name" name="name"></td>
             <td><input type="text" id="new-email" name="description"></td>
             <td><input type="text" id="new-tag" name="tag"></td>
-            <td><input type="text" id="new-image" name="imagepath"></td>
             <td><button id="newsubmit" name="newsubmit" type="submit">Inserir</button></td>
           </tr>
         </form>
@@ -88,11 +96,11 @@ function adsAdminPage()
         foreach ($results as $ads) {
           echo "
               <tr>
+                <td width='20%'><img width='80px' src='$ads->image_path'><img></td>
                 <td width='20%'>$ads->name</td>
                 <td width='20%'>$ads->description</td>
                 <td width='20%'>$ads->tag</td>
-                <td width='20%'>$ads->image_path</td>
-                <td width='20%'><a href='admin.php?page=ads.php&update=$ads->id'><button type='button'>Editar</button></a><a href='admin.php?page=ads.php&delete=$ads->id'><button type='button'>Excluir</button></a></td>
+                <td width='20%'><a href='admin.php?page=ads%2Fads.php&update=$ads->id'><button type='button'>Editar</button></a><a href='admin.php?page=ads%2Fads.php&delete=$ads->id'><button type='button'>Excluir</button></a></td>
               </tr>
             ";
         }
@@ -100,16 +108,18 @@ function adsAdminPage()
       </tbody>
     </table>
     <?php
-      if (isset($_GET['update'])) {
-        $upt_id = $_GET['update'];
-        $result = $wpdb->get_results("SELECT * FROM $table_name WHERE id='$upt_id'");
-        foreach($result as $ads) {
-          $name = $ads->name;
-          $description = $ads->description;
-          $tag = $ads->tag;
-          $image_path = $ads->image_path;
-        }
-        echo "
+    if (isset($_GET['update'])) {
+      $upt_id = $_GET['update'];
+      $result = $wpdb->get_results("SELECT * FROM $table_name WHERE id='$upt_id'");
+      foreach ($result as $ads) {
+        $name = $ads->name;
+        $description = $ads->description;
+        $tag = $ads->tag;
+        $image_path = $ads->image_path;
+      }
+      
+      echo 
+        "
         <table class='wp-list-table widefat striped'>
           <thead>
             <tr>
@@ -123,16 +133,19 @@ function adsAdminPage()
             <form action='' method='post'>
               <tr>
                 <td style='display:none'><input type='text' id='update-name' name='id' value='$ads->id'></td>
+                <td width='20%'>
+                <input type='text' id='new-image' name='imagepath' value='$ads->image_path'>
+                <input type='button' name='upload-btn' id='upload-btn' class='button-secondary' value='Upload Image'>
+                </td>
                 <td width='20%'><input type='text' id='update-name' name='name' value='$ads->name'></td>
                 <td width='20%'><input type='text' id='update-description' name='description' value='$ads->description'></td>
                 <td width='20%'><input type='text' id='update-description' name='tag' value='$ads->tag'></td>
-                <td width='20%'><input type='text' id='update-description' name='imagepath' value='$ads->image_path'></td>
-                <td width='20%'><button id='updatetsubmit' name='updatesubmit' type='submit'>UPDATE</button> <a href='admin.php?page=ads.php'><button type='button'>CANCEL</button></a></td>
+                <td width='20%'><button id='updatetsubmit' name='updatesubmit' type='submit'>UPDATE</button> <a href='admin.php?page=ads%2Fads.php'><button type='button'>CANCEL</button></a></td>
               </tr>
             </form>
           </tbody>
         </table>";
-      }
+    }
     ?>
   </div>
 <?php
