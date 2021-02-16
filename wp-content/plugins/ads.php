d28edab203f68c9a2a37693f83578bf60b2ed2a2
Pedro Henrique <pdr_castro@hotmail.com>
2021-02-15 21:14:55 -0300

    create a insert method in plugin

--- wp-content/plugins/ads.php
+++ wp-content/plugins/ads.php
@@ -1,12 +1,15 @@
 <?php
 /*
 Plugin Name: Coopersystem ads challenger
-Description: Puglin desenvolvido para cadastro e controle e anuncios para o desafio da Coopersystem.
+Description: Plugin desenvolvido para cadastro e controle de anuncios para o desafio da Coopersystem.
 Version: 1.0.0
 Author: Pedro Henrique
 */
-register_activation_hook( __FILE__, 'adsOperationsTable');
-function AdsOperationsTable() {
+
+register_activation_hook(__FILE__, 'adsOperationsTable');
+
+function AdsOperationsTable()
+{
   global $wpdb;
   $charset_collate = $wpdb->get_charset_collate();
   $table_name = $wpdb->prefix . 'ads';
@@ -25,6 +28,62 @@ function AdsOperationsTable() {
   }
 }
 add_action('admin_menu', 'addAdminPageContent');
-function addAdminPageContent() {
-  add_menu_page('ADS', 'ADS', 'manage_options' ,__FILE__, 'AdsAdminPage', 'dashicons-feedback');
+function addAdminPageContent()
+{
+  add_menu_page('ADS', 'ADS', 'manage_options', __FILE__, 'AdsAdminPage', 'dashicons-feedback');
+}
+function adsAdminPage()
+{
+  global $wpdb;
+  $table_name = $wpdb->prefix . 'ads';
+  if (isset($_POST['newsubmit'])) {
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+    $tag = $_POST['tag'];
+    $image_path = $_POST['imagepath'];
+
+    $wpdb->query("INSERT INTO $table_name (`name`, `description`, `tag`, `image_path`) VALUES ('$name', '$description', '$tag', '$image_path')");
+  }
+
+?>
+
+  <div class="wrap">
+    <h2>Gerenciar Anuncios</h2>
+    <table class="wp-list-table widefat striped">
+      <thead>
+        <tr>
+          <th width="25%">Nome</th>
+          <th width="25%">Descrição</th>
+          <th width="25%">Tag</th>
+          <th width="25%">Imagem</th>
+        </tr>
+      </thead>
+      <tbody>
+        <form action="" method="post">
+          <tr>
+            <td><input type="text" id="new-name" name="name"></td>
+            <td><input type="text" id="new-email" name="description"></td>
+            <td><input type="text" id="new-tag" name="tag"></td>
+            <td><input type="text" id="new-image" name="imagepath"></td>
+            <td><button id="newsubmit" name="newsubmit" type="submit">Inserir</button></td>
+          </tr>
+        </form>
+        <?php
+        $results = $wpdb->get_results("SELECT * FROM $table_name");
+        foreach ($results as $ads) {
+          echo "
+              <tr>
+                <td width='20%'>$ads->name</td>
+                <td width='20%'>$ads->description</td>
+                <td width='20%'>$ads->tag</td>
+                <td width='20%'>$ads->image_path</td>
+                <td width='20%'><button type='button'>Editar</button><button type='button'>Excluir</button></a></td>
+              </tr>
+            ";
+        }
+        ?>
+      </tbody>
+    </table>
+  </div>
+<?php
 }
